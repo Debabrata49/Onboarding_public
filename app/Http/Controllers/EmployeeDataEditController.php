6f2928146caa1b4c1419ca64ef4a2be71a451e2d
Debabrata Karmakar <debabrata.myewards@gmail.com>
2024-09-11 12:37:18 +0530

    chnages

--- app/Http/Controllers/EmployeeDataEditController.php
+++ app/Http/Controllers/EmployeeDataEditController.php
@@ -62,7 +62,7 @@ class EmployeeDataEditController extends Controller
                 ];
             }
         }
-        unset($pos_reg);
+        // unset($pos_reg);
         // dd($pos_reg);
         $sms_select = [];
         $sms_vendor = SmsVendor::select('id','sms_vendor_name')->where('status',1)->get();
@@ -95,9 +95,472 @@ class EmployeeDataEditController extends Controller
                 'select' => $email->id == $employee->email_vendor_id ? 1 : 0
             ];
         }
+        //Manage POS level permissions
+        $points_on_discounted_bill = [
+            [
+                'name' => 'Yes',
+                'value' => '1',
+                'select' => $employee->points_on_discounted_bill_permission == 1 ? 1 : 0
+            ],
+            [
+                'name' => 'No',
+                'value' => '0',
+                'select' => $employee->points_on_discounted_bill_permission == 0 ? 1 : 0
+            ]
+        ];
+        $coupon_redeem_transaction = [
+            [
+                'name' => 'Yes',
+                'value' => 'transaction',
+                'select' => $pos_reg->coupon_redeem_transaction == 'transaction' ? 1 : 0
+            ],
+            [
+                'name' => 'No',
+                'value' => 'checkin',
+                'select' => $pos_reg->coupon_redeem_transaction == 'checkin' ? 1 : 0
+            ]
+        ];
+        $points_on_tax = [
+            [
+                'name' => 'Yes',
+                'value' => '1',
+                'select' => $employee->points_on_tax == 1 ? 1 : 0
+            ],
+            [
+                'name' => 'No',
+                'value' => '0',
+                'select' => $employee->points_on_tax == 0 ? 1 : 0
+            ]
+        ];
+        $points_on_discounted_item  = [
+            [
+                'name' => 'Yes',
+                'value' => '1',
+                'select' => $employee->points_on_discounted_item == 1 ? 1 : 0
+            ],
+            [
+                'name' => 'No',
+                'value' => '0',
+                'select' => $employee->points_on_discounted_item == 0 ? 1 : 0
+            ]
+        ];
+        $permission_arr = explode(',', $employee->permission);
+        $redeem_add_credits_permission = [
+            [
+                'name' => 'Yes',
+                'value' => '1',
+                'select' => in_array(23,$permission_arr) ? 1 : 0
+            ],
+            [
+                'name' => 'No',
+                'value' => '0',
+                'select' => in_array(23,$permission_arr)? 1 : 0
+            ]
+        ];
+        
+        //Sub-Accounts Permission
+        $sub_accounts_permission = [
+            'add credits' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(1,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(1,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Verify code' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(2,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(2,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Pending Requests' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(4,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(4,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Redeem Credits' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(5,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(5,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Custom ewallet' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(6,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(6,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Overview' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(7,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(7,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Promo Codes' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(8,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(8,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Segments' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(9,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(9,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Insight' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(10,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(10,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Campaign' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(11,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(11,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Trend' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(12,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(12,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Reports module' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(13,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(13,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Delete transactions' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(14,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(14,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Engagements' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(15,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(15,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'OTP report' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(16,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(16,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'ADV overview' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(17,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(17,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Heat map' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(18,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(18,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'CES' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(19,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(19,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'GMS' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(20,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(20,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Feedback' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(21,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(21,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Other profile - Add member' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(22,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(22,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Redeem/add combo' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(23,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(23,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'SKU' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(24,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(24,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Retention' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(25,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(25,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Leaderboard' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(26,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(26,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'RFM Campaigns' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(27,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(27,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'GMS Verify code' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(28,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(28,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'GMS Redeem credits' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(29,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(29,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'GMS Add credits' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(30,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(30,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'OMD' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(31,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(31,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Ginesys OTP bypaas' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(32,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(32,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Wallet' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(33,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(33,$permission_arr) ? 1 : 0
+                ]
+            ],
+            'Skip OTP authentication' => [
+                [
+                    'name' => 'Yes',
+                    'value' => '1',
+                    'select' => in_array(34,$permission_arr) ? 1 : 0
+                ],
+                [
+                    'name' => 'No',
+                    'value' => '0',
+                    'select' => in_array(34,$permission_arr) ? 1 : 0
+                ]
+            ],
+
+        ];
 
         echo"<pre>";
-        print_r($pos_vendor_select);
+        print_r($sub_accounts_permission);
         // print_r($pos_reg->pos_vendor_id);
         exit;
 
